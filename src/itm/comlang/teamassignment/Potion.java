a6d6e27725674419100c5b85f50dfe7d267a8077
ohkyounghun <ohkyounghun@seoultech.ac.kr>
2025-05-25 22:57:58 +0900

    포션 클래스 완벽구현
    static = 모두가 공유하는 법칙
    final = 절대 바뀌지 않는값 (파이)
    static final = 전 인류가 공유하는 절대 바꿀 수 없는 상수
    
    public static Potion charToPotion
    이렇게 메서드 이름앞에 클래스이름을 붙이는 이유는 Potion 객체 그 자체를 리턴하기 위함.
    
    ++ 몬스터를 서브클래스로 굳이 안 나누어도 될 거 같아서 삭제함.

--- src/itm/comlang/teamassignment/Potion.java
+++ src/itm/comlang/teamassignment/Potion.java
@@ -12,8 +12,30 @@ public class Potion {
     private String potionName;
     private int healAmount;
     
-    public Potion (String potionName , int healAmount) {
+    public Potion(String potionName , int healAmount) {
         this.potionName = potionName;
         this.healAmount = healAmount;
+    }
+    
+    public String getPotionName() {
+        return this.potionName;
+    }
+    
+    public int getHealAmount() {
+        return this.healAmount;
+    }
+    
+    @Override
+    public String toString() {
+        return  this.potionName + " (+" + this.healAmount + " HP)";
+    }
+    
+    public static final Potion MINOR_FLASK = new Potion("Minor Flask" , 6);
+    public static final Potion BIG_FLASK = new Potion("Big Flask" , 12);
+    
+    public static Potion charToPotion(char c) {
+        if ( c == 'm') return MINOR_FLASK;
+        else if ( c == 'B') return BIG_FLASK;
+        else return null;
     }
 }
