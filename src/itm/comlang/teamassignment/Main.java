2cf715f2dc385aff735a39f8d5d2401b61c1f68a
ohkyounghun <ohkyounghun@seoultech.ac.kr>
2025-05-27 23:14:43 +0900

    room information update
    이클래스 공지사항에 올라온 새로운 room.csv 파일로 바꿈.

--- src/itm/comlang/teamassignment/Main.java
+++ src/itm/comlang/teamassignment/Main.java
@@ -14,9 +14,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      Room room = new Room("room1.csv");
-      room.printRoom();
-       
+        Room room1 = new Room("room1.csv");
+        Room room2 = new Room("room2.csv");
+        Room room3 = new Room("room3.csv");
+        Room room4 = new Room("room4.csv");
+        room1.printRoom();
+        room2.printRoom();
+        room3.printRoom();
+        room4.printRoom();
+
     }
-    
+
 }
