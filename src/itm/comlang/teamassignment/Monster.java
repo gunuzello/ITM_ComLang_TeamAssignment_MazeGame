35abcc34699a6855695a15c463f37b52bb6d4332
김건우 <luckdaniel@naver.com>
2025-06-02 18:46:48 +0900

    Monster 수정
    getName 추가
    각주 추가
    isDead 추가

--- src/itm/comlang/teamassignment/Monster.java
+++ src/itm/comlang/teamassignment/Monster.java
@@ -28,36 +28,56 @@ public class Monster {
         this.name = name;
     }
 
+    //몬스터의 이름 반환
+    public String getName(){
+        return this.name;
+    }
+    
+    //몬스터 위치의 x값 반환 
     public int getX() {
         return x;
     }
 
+    //몬스터 위치의 y값 반환 
     public int getY() {
         return y;
     }
 
+    //몬스터의 체력 반환 
     public int getHp() {
         return hp;
     }
 
+    //해당 몬스터가 가지는 공격력 반환 
     public int getDamage() {
         return damage;
     }
 
+    //해당 몬스터가 key를 들고 있는지 참.거짓 반환 
     public boolean isKeyHolder() {
         return keyHolder;
     }
 
-    public void takeDamage(int amount) {
-        hp -= amount;
+    //몬스터가 히어로에게 공격당했을때 받은 데미지만큼 체력 소모 
+    public void takeDamage(int damageTaken) {
+        hp -= damageTaken;
         if (hp < 0) {
             hp = 0;
         }
     }
 
-    public void attack(Hero hero) {
-        hero.takeDamage(damage);
+    //몬스터의 사망여부 확인 
+    public boolean isDead(){
+        return this.hp <=0;
     }
+    
+    
+    
+    //몬스터가 히어로를 공격할 때 실행
+    public void attack(Hero hero) {
+        hero.takeDamage(this.damage);
+    }
+    
     public String toString() {
         return this.name + " (Damage: " + this.damage + " " + " HP: " + this.hp+ ")";
     }
